4a29ab2147b35cf9fc44a23373de85149ff56079
DihanFeng <110788465+DihanFeng@users.noreply.github.com>
2022-08-07 13:35:14 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+  createCanvas(400,400);
+  //dihan = createSprite(200,200,50,50);
+  var dihan = createSprite(200,200,50,50);
+  dihan.shapeColor = "blue";
+
+}
+
+function draw() {
+  background(0);
+  drawSprites();
+  
+}
+
+
+
+
